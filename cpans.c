4908d0c4d7dc91add369172aba53a0b56215eba9
c9s <cornelius.howl@gmail.com>
2010-08-13 02:06:58 +0800

    newline

--- cpans.c
+++ cpans.c
@@ -207,16 +207,16 @@ int search(const char * pattern)
 
 void help()
 {
-    printf( "cpans usage:\n" );
-    printf( "  To init source list:\n" );
+    printf( "cpans usage:\n\n" );
+    printf( "  To init source list:\n\n" );
     printf( "     cpans -i http://cpan.nctu.edu.tw/ \n" );
     printf( "     cpans --init http://cpan.nctu.edu.tw/ \n\n" );
 
-    printf( "  To update source list:\n" );
+    printf( "  To update source list:\n\n" );
     printf( "     cpans --update  \n" );
     printf( "     cpans --u  \n\n" );
 
-    printf( "  To search:\n" );
+    printf( "  To search:\n\n" );
     printf( "     cpans [pattern]  \n\n" );
 }
 
