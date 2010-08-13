6db5153fd4a33f1f500becc7d288eb8b325c0234
c9s <cornelius.howl@gmail.com>
2010-08-13 16:36:58 +0800

    define sourcemeta_t struct

--- cpans.h
+++ cpans.h
@@ -1,7 +1,11 @@
 #ifndef __CPANS_H__
 #define __CPANS_H__
 
-
+typedef struct {
+    char listuri[80];
+    float version;
+    time_t updatetime;
+} sourcemeta_t;
 
 typedef struct {
     char name[60];
