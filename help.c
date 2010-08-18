99b0851e29b655a8db2cb050281aa2785f6015e0
shelling <navyblueshellingford@gmail.com>
2010-08-19 00:06:17 +0800

    purge trivial printf()

--- help.c
+++ help.c
@@ -2,15 +2,15 @@
 
 void help()
 {
-    printf( "cpans usage:\n\n" );
-    printf( "  To init source list:\n\n" );
-    printf( "     cpans -i http://cpan.nctu.edu.tw/ \n" );
-    printf( "     cpans --init http://cpan.nctu.edu.tw/ \n\n" );
+    printf( "cpans usage:\n\n"
+            "  To init source list:\n\n"
+            "     cpans -i http://cpan.nctu.edu.tw/ \n"
+            "     cpans --init http://cpan.nctu.edu.tw/ \n\n"
 
-    printf( "  To update source list:\n\n" );
-    printf( "     cpans --update  \n" );
-    printf( "     cpans --u  \n\n" );
+            "  To update source list:\n\n"
+            "     cpans --update  \n"
+            "     cpans --u  \n\n"
 
-    printf( "  To search:\n\n" );
-    printf( "     cpans [pattern]  \n\n" );
+            "  To search:\n\n"
+            "     cpans [pattern]  \n\n" );
 }
