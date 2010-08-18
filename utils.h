48734c32c78107e39e58e084388b04d63eec95bd
shelling <navyblueshellingford@gmail.com>
2010-08-19 00:16:01 +0800

    move slist_transform() to utils.h

--- utils.h
+++ utils.h
@@ -36,5 +36,67 @@ char * slist_url() {
 
 }
 
+void slist_transform( const char * url , const char * sourcefile ) {
+
+    sourcemeta_t smeta;
+
+    FILE *in = fopen( sourcefile , "rb" );
+    FILE *out = fopen( indexfile() , "wb+" );
+
+    char buffer[300];
+    for (int i = 0; i < 9; i++) {
+        fgets( buffer , 300 , in );   // skip 9 lines (header)
+    }
+
+    memset( &smeta , 0 , sizeof( sourcemeta_t ));
+    sprintf( smeta.uri , "%s" , url );
+    fwrite( &smeta , sizeof(sourcemeta_t) , 1 , out );
+
+
+    int cnt = 0;
+    char * s1, *s2;
+    while( !feof(in) ) {
+
+        moduledata_t mdata;
+        memset( buffer , 0 , 300 );
+        memset( &mdata , 0 , sizeof(moduledata_t) );
+        fgets( buffer , 300 , in );
+
+        if( strlen(buffer) == 0 )
+            break;
+
+        s1 = buffer;
+        s2 = buffer;
+        s2 = skipword( s2 );
+        if( s1 == s2 ) break;
+        memcpy( mdata.name , s1 , (s2-s1));
+        s2 = skipspace( s2 );
+
+        s1 = s2;
+        s2 = s2;
+        s2 = skipword( s2 );
+        if( s1 == s2 ) break;
+        memcpy( mdata.version , s1 , (s2-s1) );
+        if( strcmp(mdata.version ,"undef") == 0 )
+            strcpy(mdata.version,"0");
+        s2 = skipspace( s2 );
+
+        s1 = s2;
+        s2 = s1;
+        s2 = skipword( s2 );
+        if( s1 == s2 ) break;
+        memcpy( mdata.path , s1 , (s2-s1) );
+
+        // printf( "%s - %s - %s\n" , mdata.name , mdata.version , mdata.path );
+        fwrite( &mdata , sizeof(moduledata_t) , 1 , out  );
+        cnt++;
+    }
+
+    printf( "%d packages recorded.\n" , cnt );
+
+    fclose(out);
+    fclose(in);
+    unlink( sourcefile );
+}
 
 #endif
